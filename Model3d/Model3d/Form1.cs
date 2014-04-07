08c3d437c54f88081e7f0c3384ef3f4eb1e09022
skweper <skweper@gmail.com>
2014-04-07 08:58:04 +0300

    Add class vector.

--- Model3d/Model3d/Form1.cs
+++ Model3d/Model3d/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media.Media3D;
 
 namespace Model3d
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        class Vector
+        {
+            private int x;
+            private int y;
+            private int z;
+            public Vector(int X, int Y, int Z)
+            {
+                x = X;
+                y = Y;
+                z = Z;
+            }
+            public int X
+            {
+                get
+                {
+                    return x;
+                }
+            }
+            public int Y
+            {
+                get
+                {
+                    return y;
+                }
+            }
+            public int Z
+            {
+                get
+                {
+                    return z;
+                }
+            }
         }
     }
 }
